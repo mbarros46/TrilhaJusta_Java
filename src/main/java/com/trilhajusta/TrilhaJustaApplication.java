fb19312ea3ca57e1d5dda45ee3103a22935e712b
Miguel Barros Ramos <161357805+mbarros46@users.noreply.github.com>
2025-11-23 22:09:13 -0300

    chore: add OpenAPI and security scheme annotations

--- src/main/java/com/trilhajusta/TrilhaJustaApplication.java
+++ src/main/java/com/trilhajusta/TrilhaJustaApplication.java
@@ -1,12 +1,26 @@
 package com.trilhajusta;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@OpenAPIDefinition(
+    info = @Info(title = "TrilhaJusta API", version = "1.0", description = "Documentação da API de Java do grupo TrilhaJusta da Global Solution 2°sem. 2025")
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
 public class TrilhaJustaApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(TrilhaJustaApplication.class, args);
     }
